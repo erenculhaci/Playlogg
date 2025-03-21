7a0f1dadf5368ed1ac50012f3d2d7a994784442c
Eren Culhacı <erenculhaci.b@gmail.com>
2025-03-21 16:55:24 +0300

    front-end constraint for forms

--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, authenticate, logout
-from django.db.models import Count, F, Case, When, IntegerField, Q, Value, Avg
+from django.contrib.auth import login, logout
+from django.db.models import Count, F, Q, Avg
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
 from .models import Game, Comment, Profile, GameLog, User
 from .forms import GameLogForm, ProfileEditForm, UserEditForm
 from django.contrib import messages
@@ -12,7 +11,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 import os
 from django.contrib.auth.hashers import check_password
-from django.db import transaction
 from django.core.paginator import Paginator
 import json
 
